6ab9bab672a282bb71800581141a06ba6967ed2d
Luis Cortez <lac0084@auburn.com>
2019-03-31 15:46:51 -0500

    Added test format to all test - compiled and ran all test // all test passed

--- logictest/Class1.cs
+++ logictest/Class1.cs
@@ -3,10 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
-namespace logictest
+namespace logicTest
 {
-    public class Class1
-    {
-    }
+     [TestFixture]
+     public class logicTests
+     {
+          [Test]
+          public void HelloTest()
+          {
+               Assert.Pass();
+          }
+
+          [Test]
+          public void testFormat()
+          {
+               // Arrange test parameters
+               var input = "hello";
+               var expected = "hello";
+
+               // Action to be performed
+
+               var actual = expected;
+
+               // Assert, test
+               Assert.AreEqual(expected, actual);
+          }
+
+          [Test]
+          [TestCase("hello", "hello")]
+          [TestCase("BOOM", "BOOM")]
+          [TestCase("hello123", "hello123")]
+          public void SuccessfulScenariosTest(string input, string expectedOutput)
+          {
+               // Act
+               var actualOutput = input;
+
+               // Assert
+               Assert.AreEqual(expectedOutput, actualOutput, "Invalid value returned");
+          }
+     }
 }
