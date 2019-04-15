4415c0e54540eaedd7108bc1ae5e72811e74ee85
Luis Cortez <lac0084@auburn.com>
2019-04-14 19:06:40 -0500

    added HistoryItem.Add added HistoryItem.GetItems

--- WebBrowser.Logic/HistoryManager.cs
+++ WebBrowser.Logic/HistoryManager.cs
@@ -1,14 +1,33 @@
-﻿using WebBrowser.Data.BrowserDBDataSetTableAdapters;
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using WebBrowser.Data.BrowserDBDataSetTableAdapters;
 
 namespace WebBrowser.Logic
 {
      public class HistoryManager
      {
-          public static HistoryTableAdapter HistoryAdapter = new HistoryTableAdapter();
 
           public static void Add(HistoryItem itemIn)
           {
-               HistoryAdapter.Insert(itemIn.Url, itemIn.Title, itemIn.Date);
+               var adapter = new HistoryTableAdapter();
+               adapter.Insert(itemIn.Url, itemIn.Title, itemIn.Date);
+          }
+
+          public static List<HistoryItem> GetItems()
+          {
+               var adapter = new HistoryTableAdapter();
+               var results = new List<HistoryItem>();
+               var rows = adapter.GetData();
+
+               foreach (var row in rows)
+               {
+                    var item = new HistoryItem {Id = row.Id, Url = row.URL, Title = row.Title, Date = row.Date};
+
+                    results.Add(item);
+
+               }
+
+               return results;
           }
      }
 }
