243271a569404fab53d0ed1610d00c54509b1d13
Luis Cortez <lac0084@auburn.com>
2019-04-14 19:49:39 -0500

    added BookmarkManagerForm

---
+++ WebBrowser.UI/BookmarkManagerForm.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace WebBrowser.UI
+{
+     partial class BookmarkManagerForm
+     {
+          /// <summary>
+          /// Required designer variable.
+          /// </summary>
+          private System.ComponentModel.IContainer components = null;
+
+          /// <summary>
+          /// Clean up any resources being used.
+          /// </summary>
+          /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+          protected override void Dispose(bool disposing)
+          {
+               if (disposing && (components != null))
+               {
+                    components.Dispose();
+               }
+               base.Dispose(disposing);
+          }
+
+          #region Windows Form Designer generated code
+
+          /// <summary>
+          /// Required method for Designer support - do not modify
+          /// the contents of this method with the code editor.
+          /// </summary>
+          private void InitializeComponent()
+          {
+               this.bookmarkItemListBox = new System.Windows.Forms.ListBox();
+               this.SuspendLayout();
+               // 
+               // bookmarkItemListBox
+               // 
+               this.bookmarkItemListBox.FormattingEnabled = true;
+               this.bookmarkItemListBox.Location = new System.Drawing.Point(0, 0);
+               this.bookmarkItemListBox.Name = "bookmarkItemListBox";
+               this.bookmarkItemListBox.Size = new System.Drawing.Size(120, 95);
+               this.bookmarkItemListBox.TabIndex = 0;
+               // 
+               // BookmarkManagerForm
+               // 
+               this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+               this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+               this.ClientSize = new System.Drawing.Size(800, 450);
+               this.Controls.Add(this.bookmarkItemListBox);
+               this.Name = "BookmarkManagerForm";
+               this.Text = "Bookmarks";
+               this.Load += new System.EventHandler(this.BookmarkManagerForm_Load);
+               this.ResumeLayout(false);
+
+          }
+
+          #endregion
+
+          private System.Windows.Forms.ListBox bookmarkItemListBox;
+     }
+}
---
+++ WebBrowser.UI/BookmarkManagerForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WebBrowser.Logic;
+
+namespace WebBrowser.UI
+{
+     public partial class BookmarkManagerForm : Form
+     {
+          public BookmarkManagerForm()
+          {
+               InitializeComponent();
+          }
+
+          private void BookmarkManagerForm_Load(object sender, EventArgs e)
+          {
+               var bookmarkItems = BookmarkManager.GetBookmarkItems();
+               foreach (var item in bookmarkItems)
+               {
+                    bookmarkItemListBox.Items.Add("Name: " + item.Title + " URL: " + item.Url);
+               }
+          }
+     }
+}
